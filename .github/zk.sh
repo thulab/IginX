944cfda739aaf18c1889236088faeed08d7afccc
zhuyuqing <yuqing.zhu@139.com>
2021-03-19 21:13:21 +0800

    Delete zk.sh

--- .github/zk.sh
+++
@@ -1,6 +0,0 @@
-wget "https://mirror.softaculous.com/apache/zookeeper/zookeeper-3.6.2/apache-zookeeper-3.6.2-bin.tar.gz"
-tar -xzf apache-zookeeper-3.6.2-bin.tar.gz
-mv apache-zookeeper-3.6.2 zookeeper
-cp zookeeper/conf/zoo_sample.cfg zookeeper/conf/zoo.cfg
-zookeeper/bin/zkServer.sh start
-telnet 127.0.0.1:2181
