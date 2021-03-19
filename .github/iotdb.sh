90ed08bb12877c588daf8ecfbce317fe6a6689fc
zhuyuqing <yuqing.zhu@139.com>
2021-03-20 02:10:58 +0800

    Add files via upload

---
+++ .github/iotdb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+sh -c "wget -nv https://apache.mirror.digionline.de/iotdb/0.11.2/apache-iotdb-0.11.2-bin.zip"
+
+sh -c "unzip apache-iotdb-0.11.2-bin.zip"
+
+sh -c "mv apache-iotdb-0.11.2-bin iotdb"
+
+sh -c "nohup iotdb/sbin/start-server.sh &"
