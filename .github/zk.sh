3d3fba4ff066bc54239e9278fb12bc54ef169fe0
zhuyuqing <yuqing.zhu@139.com>
2021-03-19 21:23:27 +0800

    Delete zk.sh

--- .github/zk.sh
+++
@@ -1,5 +0,0 @@
-tar -xzf apache-zookeeper-3.6.2-bin.tar.gz
-mv apache-zookeeper-3.6.2 zookeeper
-cp zookeeper/conf/zoo_sample.cfg zookeeper/conf/zoo.cfg 
-zookeeper/bin/zkServer.sh start
-telnet 127.0.0.1:2181
