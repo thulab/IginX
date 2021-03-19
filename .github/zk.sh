d67da8692b097de9442bea5705976bc752b86676
zhuyuqing <yuqing.zhu@139.com>
2021-03-20 01:56:00 +0800

    Update zk.sh

--- .github/zk.sh
+++ .github/zk.sh
@@ -6,7 +6,7 @@ sh -c "wget -nv https://mirror.softaculous.com/apache/zookeeper/zookeeper-3.6.2/
 
 sh -c "tar -xzf apache-zookeeper-3.6.2-bin.tar.gz"
 
-sh -c "mv apache-zookeeper-3.6.2 zookeeper"
+sh -c "mv apache-zookeeper-3.6.2-bin zookeeper"
 
 sh -c "cp zookeeper/conf/zoo_sample.cfg zookeeper/conf/zoo.cfg"
 
