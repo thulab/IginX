f186b1a344ac8f6ee4abb457dee72b8f04665ebd
zhuyuqing <yuqing.zhu@139.com>
2021-03-20 02:24:34 +0800

    Update iotdb.sh

--- .github/iotdb.sh
+++ .github/iotdb.sh
@@ -4,8 +4,6 @@ set -e
 
 sh -c "wget -nv https://apache.mirror.digionline.de/iotdb/0.11.2/apache-iotdb-0.11.2-bin.zip"
 
-sh -c "unzip apache-iotdb-0.11.2-bin.zip"
-
-sh -c "mv apache-iotdb-0.11.2 iotdb"
+sh -c "unzip apache-iotdb-0.11.2-bin.zip; mv apache-iotdb-0.11.2 iotdb"
 
 sh -c "nohup iotdb/sbin/start-server.sh &"
