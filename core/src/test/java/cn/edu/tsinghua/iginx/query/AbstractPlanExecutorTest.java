f6219a9768984bdb65e1a922a324c5dbd6332b05
iznauy <161250220@smail.nju.edu.cn>
2021-03-25 16:25:56 +0800

    fix bug in async task queue

---
+++ core/src/test/java/cn/edu/tsinghua/iginx/query/AbstractPlanExecutorTest.java
@@ -0,0 +1,6 @@
+package cn.edu.tsinghua.iginx.query;/**
+ * Created on 25/03/2021.
+ * Description: 
+ * @author iznauy
+ */public class AbstractPlanExecutorTest {
+}
---
+++ core/src/test/java/cn/edu/tsinghua/iginx/query/async/queue/MemoryAsyncTaskQueueTest.java
@@ -0,0 +1,6 @@
+package cn.edu.tsinghua.iginx.query.async.queue;/**
+ * Created on 25/03/2021.
+ * Description: 
+ * @author iznauy
+ */public class MemoryAsyncTaskQueueTest {
+}
