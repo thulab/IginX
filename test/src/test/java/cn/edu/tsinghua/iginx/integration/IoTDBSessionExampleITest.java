4b275ac020d717f14540282205cd2e4233495711
unknown <thss15_yit@163.com>
2021-04-19 12:21:56 +0800

    Finish query and aggr test

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/IoTDBSessionExampleITest.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/IoTDBSessionExampleITest.java
@@ -1,7 +1,5 @@
 package cn.edu.tsinghua.iginx.integration;
 
-import cn.edu.tsinghua.iginx.Iginx;
-import cn.edu.tsinghua.iginx.conf.Constants;
 import cn.edu.tsinghua.iginx.exceptions.ExecutionException;
 import cn.edu.tsinghua.iginx.exceptions.SessionException;
 import cn.edu.tsinghua.iginx.session.Session;
@@ -13,10 +11,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -31,7 +26,7 @@ public class IoTDBSessionExampleITest {
     private static final String COLUMN_D3_S1 = "sg1.d3.s1";
     private List<String> paths = new ArrayList<>();
 
-    private static final long timePeriod = 1000L;
+    private static final long timePeriod = 100000L;
 
     private static final long startTime = 0L;
     private static final long endTime = startTime + timePeriod - 1;
@@ -39,15 +34,15 @@ public class IoTDBSessionExampleITest {
     @Before
     public void setUp(){
         try {
+            paths.add(COLUMN_D1_S1);
+            paths.add(COLUMN_D1_S2);
+            paths.add(COLUMN_D2_S1);
+            paths.add(COLUMN_D3_S1);
             session = new Session("127.0.0.1", 6324, "root", "root");
             session.openSession();
             session.createDatabase(DATABASE_NAME);
             addColumns();
             insertRecords();
-            paths.add(COLUMN_D1_S1);
-            paths.add(COLUMN_D1_S2);
-            paths.add(COLUMN_D2_S1);
-            paths.add(COLUMN_D3_S1);
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -93,9 +88,6 @@ public class IoTDBSessionExampleITest {
         }
     }
 
-    //TODO make the insert time DISORDER
-
-    //TODO make order and disorder example in the test. Try to construct a function with its value disordered
     @Test
     public void aggrMaxTest() throws SessionException {
 
@@ -244,9 +236,7 @@ public class IoTDBSessionExampleITest {
 
     @Test
     public void aggrSumTest() throws SessionException {
-
         SessionAggregateQueryDataSet sumDataSet = session.aggregateQuery(paths, startTime, endTime + 1, AggregateType.SUM);
-        sumDataSet.print();
         assertNull(sumDataSet.getTimestamps());
         List<String> resPaths = sumDataSet.getPaths();
         Object[] result = sumDataSet.getValues();
@@ -268,7 +258,6 @@ public class IoTDBSessionExampleITest {
                     assertEquals(result[i], sum + timePeriod * 3);
                     break;
                 default:
-                    System.out.println();
                     fail();
                     break;
             }
@@ -286,18 +275,19 @@ public class IoTDBSessionExampleITest {
         assertEquals(resPaths.size(), 4);
         assertEquals(avgDataSet.getValues().length, 4);
         for(int i = 0; i < 4; i++) {
+            double avg = (startTime + endTime) / 2.0;
             switch (resPaths.get(i)){
                 case "root.sg1.d1.s1":
-                    assertEquals(result[i], (startTime + endTime) / 2.0);
+                    assertEquals(result[i], avg);
                     break;
                 case "root.sg1.d1.s2":
-                    assertEquals(result[i],  (startTime + endTime) / 2.0 + 1);
+                    assertEquals(result[i],  avg + 1);
                     break;
                 case "root.sg1.d2.s1":
-                    assertEquals(result[i], (startTime + endTime) / 2.0 + 2);
+                    assertEquals(result[i], avg + 2);
                     break;
                 case "root.sg1.d3.s1":
-                    assertEquals(result[i], (startTime + endTime) / 2.0 + 3);
+                    assertEquals(result[i], avg + 3);
                     break;
                 default:
                     fail();
@@ -306,8 +296,6 @@ public class IoTDBSessionExampleITest {
         }
     }
 
-    // TODO combine the delete into different aggregate functions
-
     @Test
     public void deletePathTest() throws SessionException {
         List<String> delPaths = new ArrayList<>();
@@ -315,8 +303,10 @@ public class IoTDBSessionExampleITest {
         delPaths.add(COLUMN_D2_S1);
         delPaths.add(COLUMN_D3_S1);
 
-        long delStartTime = 25L;
-        long delEndTime = 30L;
+        // ensure after delete there are still points in the timeseries
+        long delStartTime = 20000L;
+        long delEndTime = 90000L;
+        long delTimePeriod = delEndTime - delStartTime + 1;
 
         session.deleteDataInColumns(delPaths, delStartTime, delEndTime);
 
@@ -359,6 +349,115 @@ public class IoTDBSessionExampleITest {
                             break;
                     }
                 }
+            }
+        }
+
+        // Test avg for the delete
+        SessionAggregateQueryDataSet avgDataSet = session.aggregateQuery(paths, startTime, endTime + 1, AggregateType.AVG);
+        List<String> avgResPaths = avgDataSet.getPaths();
+        Object[] avgResult = avgDataSet.getValues();
+        assertEquals(avgResPaths.size(), 4);
+        assertEquals(avgDataSet.getValues().length, 4);
+
+        for(int i = 0; i < 4; i++) {
+            double avg = ((startTime + endTime) * timePeriod / 2.0
+                    - (delStartTime + delEndTime) * delTimePeriod / 2.0) / (timePeriod - delTimePeriod);
+            switch (avgResPaths.get(i)){
+                case "root.sg1.d1.s1":
+                    assertEquals(avgResult[i], avg);
+                    break;
+                case "root.sg1.d1.s2":
+                    assertEquals(avgResult[i],(startTime + endTime) / 2.0 + 1);
+                    break;
+                case "root.sg1.d2.s1":
+                    assertEquals(avgResult[i],avg + 2);
+                    break;
+                case "root.sg1.d3.s1":
+                    assertEquals(avgResult[i],avg + 3);
+                    break;
+                default:
+                    fail();
+                    break;
+            }
+        }
+
+        // Test max for the delete
+        SessionAggregateQueryDataSet maxDataSet = session.aggregateQuery(paths, startTime, endTime + 1, AggregateType.MAX);
+        List<String> maxResPaths = maxDataSet.getPaths();
+        Object[] maxResult = maxDataSet.getValues();
+        assertEquals(maxResPaths.size(), 4);
+        assertEquals(maxDataSet.getValues().length, 4);
+
+        for(int i = 0; i < 4; i++) {
+            long max = (delEndTime >= endTime) ? delStartTime - 1 : endTime;
+            switch (maxResPaths.get(i)){
+                case "root.sg1.d1.s1":
+                    assertEquals(maxResult[i], max);
+                    break;
+                case "root.sg1.d1.s2":
+                    assertEquals(maxResult[i], endTime + 1);
+                    break;
+                case "root.sg1.d2.s1":
+                    assertEquals(maxResult[i], max + 2);
+                    break;
+                case "root.sg1.d3.s1":
+                    assertEquals(maxResult[i], max + 3);
+                    break;
+                default:
+                    fail();
+                    break;
+            }
+        }
+    }
+
+    @Test
+    public void deleteAllDataInColumnTest() throws SessionException {
+        List<String> delPaths = new ArrayList<>();
+        delPaths.add(COLUMN_D1_S1);
+        delPaths.add(COLUMN_D2_S1);
+        delPaths.add(COLUMN_D3_S1);
+
+        session.deleteDataInColumns(delPaths, startTime, endTime);
+
+        SessionQueryDataSet dataSet = session.queryData(paths, startTime, endTime);
+
+        int len = dataSet.getTimestamps().length;
+        List<String> resPaths = dataSet.getPaths();
+        assertEquals(resPaths.size(), 4);
+        assertEquals(dataSet.getTimestamps().length, timePeriod);
+        assertEquals(dataSet.getValues().size(), timePeriod);
+        for (int i = 0; i < len; i++){
+            long timestamp = dataSet.getTimestamps()[i];
+            assertEquals(timestamp, i);
+            List<Object> result = dataSet.getValues().get(i);
+            for (int j = 0; j < 4; j++) {
+                if ("root.sg1.d1.s2".equals(resPaths.get(j))) {
+                    assertEquals(result.get(j), (long) (i + 1));
+                } else {
+                    assertNull(result.get(j));
+                }
+            }
+        }
+
+        // Test aggregate function for the delete
+        SessionAggregateQueryDataSet avgDataSet = session.aggregateQuery(paths, startTime, endTime + 1, AggregateType.AVG);
+        List<String> avgResPaths = avgDataSet.getPaths();
+        Object[] avgResult = avgDataSet.getValues();
+        assertEquals(avgResPaths.size(), 4);
+        assertEquals(avgDataSet.getValues().length, 4);
+        for(int i = 0; i < 4; i++) {
+            switch (avgResPaths.get(i)){
+                case "root.sg1.d1.s2":
+                    assertEquals(avgResult[i],(startTime + endTime) / 2.0 + 1);
+                    break;
+                case "root.sg1.d2.s1":
+                case "root.sg1.d1.s1":
+                case "root.sg1.d3.s1":
+                    assertEquals(new String((byte[]) avgResult[i]), "null");
+                    break;
+                default:
+                    fail();
+                    break;
             }
         }
     }
@@ -444,5 +543,4 @@ public class IoTDBSessionExampleITest {
 
         session.insertColumnRecords(insertPaths, timestamps, valuesList, dataTypeList, null);
     }
-
 }
