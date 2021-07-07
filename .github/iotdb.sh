7e62e5d868fd08dcd24ef1bc10588839e9fefc07
unknown <thss15_yit@163.com>
2021-07-07 23:50:05 +0800

    fix directory

--- .github/iotdb.sh
+++ .github/iotdb.sh
@@ -14,7 +14,7 @@ sh -c "echo ========================="
 
 sh -c "sudo cp -r apache-iotdb-0.11.3 apache-iotdb2-0.11.3"
 
-sed -i "s/6667/6668/g" apache-iotdb2-0.11.3/conf/config.properties
+sed -i "s/6667/6668/g" apache-iotdb2-0.11.3/conf/iotdb-engine.properties
 
 sh -c "ls apache-iotdb-0.11.3"
 
