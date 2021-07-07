326e1c18ab14154bd1a27c0e6a21b83c91076695
unknown <thss15_yit@163.com>
2021-07-08 03:03:39 +0800

    fix

--- .github/iotdb.sh
+++ .github/iotdb.sh
@@ -14,7 +14,7 @@ sh -c "sudo cp -r apache-iotdb-0.11.3/ apache-iotdb2-0.11.3/"
 
 sh -c "sudo sed -i 's/6667/6668/g' apache-iotdb2-0.11.3/conf/iotdb-engine.properties"
 
-sh -c "cd apache-iotdb-0.11.3/; nohup sbin/start-server.sh &"
+sudo sh -c "cd apache-iotdb-0.11.3/; nohup sbin/start-server.sh &"
 
-sh -c "cd apache-iotdb2-0.11.3/; nohup sbin/start-server.sh &"
+sudo sh -c "cd apache-iotdb2-0.11.3/; nohup sbin/start-server.sh &"
 
