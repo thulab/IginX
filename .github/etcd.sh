951bdccca2b2439b7746dc11ba1311e323e8ff7c
iznauy <161250220@smail.nju.edu.cn>
2021-07-08 22:11:19 +0800

    fix etcd

--- .github/etcd.sh
+++ .github/etcd.sh
@@ -14,18 +14,10 @@ export GOBIN=$GOPATH/bin
 
 export PATH=$PATH:$GOBIN
 
-echo $PATH
-
-echo $GOROOT
-
-echo $GOBIN
-
-echo $GOPATH
-
 sh -c "wget -nv https://github.com/etcd-io/etcd/releases/download/v3.5.0/etcd-v3.5.0-linux-amd64.tar.gz"
 
 sh -c "tar -zxvf etcd-v3.5.0-linux-amd64.tar.gz"
 
 sh -c "mv etcd-v3.5.0-linux-amd64/etcd* $GOBIN"
 
-sh -c "etcd"
+sh -c "nohup etcd >> run.log 2>&1 &"
