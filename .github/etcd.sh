b3410c1162f87720cb4e65c81dc6b5f215d499ef
iznauy <161250220@smail.nju.edu.cn>
2021-07-08 21:53:08 +0800

    fix etcd

--- .github/etcd.sh
+++ .github/etcd.sh
@@ -12,6 +12,12 @@ sh -c "export GOPATH=$GOROOT/bin"
 
 sh -c "export PATH=$PATH:$GOPATH"
 
+sh -c "echo $PATH"
+
+sh -c "echo $GOROOT"
+
+sh -c "echo $GOPATH"
+
 sh -c "wget -nv https://github.com/etcd-io/etcd/releases/download/v3.5.0/etcd-v3.5.0-linux-amd64.tar.gz"
 
 sh -c "tar -zxvf etcd-v3.5.0-linux-amd64.tar.gz"
