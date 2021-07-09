70f29e7b16f0619246fb85522fb16db7d50a1987
iznauy <161250220@smail.nju.edu.cn>
2021-07-09 23:33:18 +0800

    fix test

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/IMetaManagerTest.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/IMetaManagerTest.java
@@ -18,6 +18,9 @@
  */
 package cn.edu.tsinghua.iginx.integration;
 
+import cn.edu.tsinghua.iginx.conf.ConfigDescriptor;
+import cn.edu.tsinghua.iginx.metadata.DefaultMetaManager;
+import cn.edu.tsinghua.iginx.metadata.IMetaManager;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -26,14 +29,28 @@ import org.junit.Test;
 
 public class IMetaManagerTest {
 
+    private static IMetaManager iMetaManager;
+
     @BeforeClass
     public static void beforeClass() {
-
+        switch (System.getenv("STORAGE")) {
+            case "zookeeper":
+                ConfigDescriptor.getInstance().getConfig().setMetaStorage("zookeeper");
+                ConfigDescriptor.getInstance().getConfig().setZookeeperConnectionString(System.getenv("ZOOKEEPER_CONNECTION_STRING"));
+                System.out.println("use zookeeper as meta storage engine");
+                break;
+            case "etcd":
+                ConfigDescriptor.getInstance().getConfig().setMetaStorage("etcd");
+                ConfigDescriptor.getInstance().getConfig().setEtcdEndpoints(System.getenv("ETCD_ENDPOINTS"));
+                System.out.println("use etcd as meta storage engine");
+                break;
+        }
+        iMetaManager = DefaultMetaManager.getInstance();
     }
 
     @AfterClass
     public static void afterClass() {
-
+        iMetaManager = null;
     }
 
     @Before
@@ -50,6 +67,26 @@ public class IMetaManagerTest {
     public void schemaMappingTest() {
         System.out.println("Test Schema Mapping");
         System.out.printf("env: STORAGE=%s%n", System.getenv("STORAGE"));
+    }
+
+    @Test
+    public void iginxTest() {
+
+    }
+
+    @Test
+    public void storageEngineTest() {
+
+    }
+
+    @Test
+    public void fragmentTest() {
+
+    }
+
+    @Test
+    public void storageUnitTest() {
+
     }
 
 }
