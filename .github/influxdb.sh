d485dc0fcc46fb5873941e6d956a933fc556448f
unknown <thss15_yit@163.com>
2021-07-14 17:40:29 +0800

    sleep

--- .github/influxdb.sh
+++ .github/influxdb.sh
@@ -12,7 +12,7 @@ sh -c "sudo cp -r influxdb2-2.0.7-linux-amd64/ influxdb2-2.0.7-linux-amd64-2/"
 
 sudo sh -c "cd influxdb2-2.0.7-linux-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 &"
 
-sh -c "sleep 10"
+sh -c "sleep 30"
 
 sh -c "./influxdb2-2.0.7-linux-amd64/influx setup --org testOrg --bucket testBucket --username user --password 12345678 --force"
 
