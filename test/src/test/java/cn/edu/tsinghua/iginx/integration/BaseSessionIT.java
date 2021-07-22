3241246bb74ecc21f140c890819019520f566934
unknown <thss15_yit@163.com>
2021-07-22 10:58:07 +0800

    try more sleep

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/BaseSessionIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/BaseSessionIT.java
@@ -1199,7 +1199,7 @@ public abstract class BaseSessionIT {
         for (int i = 0; i < 5; i++) {
             mulStQueryThreads[i].join();
         }
-        Thread.sleep(30000);
+        Thread.sleep(300000);
         // TODO change the simple query and one of the avg query to multithread
         try {
         for (int i = 0; i < 5; i++) {
