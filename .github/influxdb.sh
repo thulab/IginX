e6e2faa167f5a027f15d2971242610282eedb1b9
Yi Tao <thss15_yit@163.com>
2021-07-26 10:22:05 +0800

    Add session test part of influxdb (#92)
    * Merge as a same Sessiontest, add part of influxdb

--- .github/influxdb.sh
+++ .github/influxdb.sh
@@ -8,23 +8,13 @@ sh -c "tar -zxvf influxdb2-2.0.7-linux-amd64.tar.gz"
 
 sh -c "ls influxdb2-2.0.7-linux-amd64"
 
-sh -c "sudo cp influxdb2-2.0.7-linux-amd64/influx /usr/local/bin/"
+sudo sh -c "cd influxdb2-2.0.7-linux-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 --query-memory-bytes=20971520 &"
 
-sh -c "sudo cp influxdb2-2.0.7-linux-amd64/influxd /usr/local/bin/"
+sh -c "sleep 30"
 
-sh -c "wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.7-amd64.deb"
+sh -c "./influxdb2-2.0.7-linux-amd64/influx setup --org testOrg --bucket testBucket --username user --password 12345678 --force"
 
-sh -c "sudo dpkg -i influxdb2-2.0.7-amd64.deb"
-
-sh -c "sudo service influxdb start"
-
-sh -c "sudo service influxdb status"
-
-sh -c "sleep 20"
-
-sh -c "influx setup --org testOrg --bucket testBucket --username user --password 12345678 --force"
-
-sh -c "influx auth list --json > token.json"
+sh -c "./influxdb2-2.0.7-linux-amd64/influx auth list --json > token.json"
 
 a=$(cat token.json | sed 's/,/\n/g' | grep "token" | sed 's/: /\n/g' | sed '1d' | sed '/^"token/,$d' | sed 's/\"//g')
 
@@ -32,7 +22,11 @@ sed -i "s/your-token/${a}/g" conf/config.properties
 
 sed -i "s/my-org/testOrg/g" conf/config.properties
 
+
 sed -i "s/storageEngineList=127.0.0.1#6667#iotdb/#storageEngineList=127.0.0.1#6667#iotdb/g" conf/config.properties
 
 sed -i "s/#storageEngineList=127.0.0.1#8086#influxdb/storageEngineList=127.0.0.1#8086#influxdb/g" conf/config.properties
 
+sh -c "sudo cp -r influxdb2-2.0.7-linux-amd64/ influxdb2-2.0.7-linux-amd64-2/"
+
+sudo sh -c "cd influxdb2-2.0.7-linux-amd64-2/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8087 --query-memory-bytes=20971520 &"
