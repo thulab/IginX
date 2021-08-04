1589fd790dde087383f254a352f8b94c21f3367a
SolomonAnn <thss15_anyz@163.com>
2021-08-04 11:07:47 +0800

    Modify shell

--- .github/influxdb.sh
+++ .github/influxdb.sh
@@ -22,6 +22,8 @@ sed -i "s/your-token/\"${a}\"/g" conf/config.properties
 
 sed -i "s/your-organization/testOrg/g" conf/config.properties
 
+head -100 conf/config.properties
+
 sh -c "sudo cp -r influxdb2-2.0.7-linux-amd64/ influxdb2-2.0.7-linux-amd64-2/"
 
 sudo sh -c "cd influxdb2-2.0.7-linux-amd64-2/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8087 --query-memory-bytes=20971520 &"
