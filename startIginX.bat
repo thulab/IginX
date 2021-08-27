2ae1bb0d2c9ed0f4d9cc6b88c5f082c0a382aa56
unknown <yuqing.zhu@139.com>
2021-08-27 10:43:18 +0800

    updated windows script

--- startIginX.bat
+++ startIginX.bat
@@ -4,6 +4,7 @@ echo Starting IginX
 echo ````````````````````````
 
 
+set "IGINX_VERSION=0.3.0-SNAPSHOT"
 set PATH="%JAVA_HOME%\bin\";%PATH%
 set "FULL_VERSION="
 set "MAJOR_VERSION="
@@ -70,11 +71,9 @@ set HEAP_OPTS=-Xmx%MAX_HEAP_SIZE% -Xms%HEAP_NEWSIZE% -Xloggc:"%IGINX_HOME%\gc.lo
 @REM ***** CLASSPATH library setting *****
 @REM Ensure that any user defined CLASSPATH variables are not used on startup
 set CLASSPATH="%IGINX_HOME%\core\lib\*"
-set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\core\target\iginx-core-0.1.0-SNAPSHOT.jar"
-set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\thrift\lib\*"
-set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\thrift\target\iginx-thrift-0.1.0-SNAPSHOT.jar"
-set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\iotdb\target\iotdb-0.1.0-SNAPSHOT.jar"
-set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\influxdb\target\influxdb-0.1.0-SNAPSHOT.jar"
+set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\core\target\iginx-core-%IGINX_VERSION%.jar"
+set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\iotdb\target\iotdb-%IGINX_VERSION%.jar"
+set CLASSPATH=%CLASSPATH%;"%IGINX_HOME%\influxdb\target\influxdb-%IGINX_VERSION%.jar"
 goto okClasspath
 
 :append
