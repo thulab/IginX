4a2c66ad6cbbe80f50ce67159100f17a14257704
Yuan Zi <zy05160516@126.com>
2021-09-02 21:44:34 +0800

    add file support for auth

--- core/src/main/java/cn/edu/tsinghua/iginx/metadata/storage/file/FileMetaStorage.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/metadata/storage/file/FileMetaStorage.java
@@ -17,6 +17,7 @@ import cn.edu.tsinghua.iginx.metadata.entity.StorageUnitMeta;
 import cn.edu.tsinghua.iginx.metadata.entity.TimeSeriesInterval;
 import cn.edu.tsinghua.iginx.metadata.utils.JsonUtils;
 import com.google.gson.reflect.TypeToken;
+import com.sun.org.apache.regexp.internal.RE;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,6 +57,8 @@ public class FileMetaStorage implements IMetaStorage {
 
     private static final String ID_FILE = "id.log";
 
+    private static final String USER_META_FILE = "user.log";
+
     private static final long ID_INTERVAL = 100000;
 
     private static final String UPDATE = "update";
@@ -73,6 +76,8 @@ public class FileMetaStorage implements IMetaStorage {
     private StorageUnitChangeHook storageUnitChangeHook = null;
 
     private FragmentChangeHook fragmentChangeHook = null;
+
+    private UserChangeHook userChangeHook = null;
 
     private AtomicLong idGenerator = null; // 加载完数据之后赋值
 
@@ -105,6 +110,9 @@ public class FileMetaStorage implements IMetaStorage {
             }
             if (Files.notExists(Paths.get(PATH, STORAGE_UNIT_META_FILE))) {
                 Files.createFile(Paths.get(PATH, STORAGE_UNIT_META_FILE));
+            }
+            if (Files.notExists(Paths.get(PATH, USER_META_FILE))) {
+                Files.createFile(Paths.get(PATH, USER_META_FILE));
             }
         } catch (IOException e) {
             logger.error("encounter error when create log file: ", e);
@@ -393,26 +401,79 @@ public class FileMetaStorage implements IMetaStorage {
 
     @Override
     public List<UserMeta> loadUser(UserMeta userMeta) throws MetaStorageException {
-        return null;
+        Map<String, UserMeta> users = new HashMap<>();
+
+        File userLogFile = Paths.get(PATH, USER_META_FILE).toFile();
+        if (userLogFile.length() == 0L) { // 是第一次启动
+            addUser(userMeta);
+            users.put(userMeta.getUsername(), userMeta);
+        } else {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Paths.get(PATH, STORAGE_META_FILE).toFile())))) {
+                String line;
+                String[] params;
+                while ((line = reader.readLine()) != null) {
+                    params = line.split(" ");
+                    if (params[0].equals(UPDATE)) {
+                        UserMeta user = JsonUtils.fromJson(params[1].getBytes(StandardCharsets.UTF_8), UserMeta.class);
+                        users.put(user.getUsername(), user);
+                    } else if (params[0].equals(REMOVE)) {
+                        String username = params[1];
+                        users.remove(username);
+                    } else {
+                        logger.error("unknown log content: " + line);
+                    }
+                }
+            } catch (IOException e) {
+                logger.error("encounter error when read user log file: ", e);
+                throw new MetaStorageException(e);
+            }
+        }
+        return new ArrayList<>(users.values());
     }
 
     @Override
     public void registerUserChangeHook(UserChangeHook hook) {
-
+        if (hook != null) {
+            userChangeHook = hook;
+        }
     }
 
     @Override
     public void addUser(UserMeta userMeta) throws MetaStorageException {
-
+        try (PrintWriter writer = new PrintWriter(new FileWriter(Paths.get(PATH, USER_META_FILE).toFile(), true))) {
+            writer.write(String.format("%s %s\n", UPDATE, JsonUtils.getGson().toJson(userMeta)));
+        } catch (IOException e) {
+            logger.error("write user file error: ", e);
+            throw new MetaStorageException(e);
+        }
+        if (userChangeHook != null) {
+            userChangeHook.onChange(userMeta.getUsername(), userMeta);
+        }
     }
 
     @Override
     public void updateUser(UserMeta userMeta) throws MetaStorageException {
-
+        try (PrintWriter writer = new PrintWriter(new FileWriter(Paths.get(PATH, USER_META_FILE).toFile(), true))) {
+            writer.write(String.format("%s %s\n", UPDATE, JsonUtils.getGson().toJson(userMeta)));
+        } catch (IOException e) {
+            logger.error("write user file error: ", e);
+            throw new MetaStorageException(e);
+        }
+        if (userChangeHook != null) {
+            userChangeHook.onChange(userMeta.getUsername(), userMeta);
+        }
     }
 
     @Override
     public void removeUser(String username) throws MetaStorageException {
-
+        try (PrintWriter writer = new PrintWriter(new FileWriter(Paths.get(PATH, USER_META_FILE).toFile(), true))) {
+            writer.write(String.format("%s %s\n", REMOVE, username));
+        } catch (IOException e) {
+            logger.error("write user file error: ", e);
+            throw new MetaStorageException(e);
+        }
+        if (userChangeHook != null) {
+            userChangeHook.onChange(username, null);
+        }
     }
 }
