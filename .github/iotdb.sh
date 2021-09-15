5b952e377f3db43cd9214817ffb8dd39fa4a5e26
Jensen Wang <1092792221@qq.com>
2021-09-15 22:57:29 +0800

    add iotdb 0.12 driver * change default iotdb driver to 0.11 iotdb driver * add iotdb 0.12 driver

--- .github/iotdb.sh
+++ .github/iotdb.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-sh -c "wget -nv https://mirrors.ocf.berkeley.edu/apache/iotdb/0.11.4/apache-iotdb-0.11.4-bin.zip"
+sh -c "wget -nv https://mirrors.ocf.berkeley.edu/apache/iotdb/0.12.2/apache-iotdb-0.12.2-server-bin.zip"
 
-sh -c "unzip apache-iotdb-0.11.4-bin.zip"
+sh -c "unzip apache-iotdb-0.12.2-server-bin.zip"
 
 sh -c "sleep 10"
 
@@ -12,12 +12,12 @@ sh -c "ls ./"
 
 sh -c "echo ========================="
 
-sh -c "ls apache-iotdb-0.11.4"
+sh -c "ls apache-iotdb-0.12.2-server-bin"
 
-sh -c "sudo cp -r apache-iotdb-0.11.4/ apache-iotdb2-0.11.4/"
+sh -c "sudo cp -r apache-iotdb-0.12.2-server-bin/ apache-iotdb2-0.12.2-server-bin/"
 
-sh -c "sudo sed -i 's/6667/6668/g' apache-iotdb2-0.11.4/conf/iotdb-engine.properties"
+sh -c "sudo sed -i 's/6667/6668/g' apache-iotdb2-0.12.2-server-bin/conf/iotdb-engine.properties"
 
-sudo sh -c "cd apache-iotdb-0.11.4/; nohup sbin/start-server.sh &"
+sudo sh -c "cd apache-iotdb-0.12.2-server-bin/; nohup sbin/start-server.sh &"
 
-sudo sh -c "cd apache-iotdb2-0.11.4/; nohup sbin/start-server.sh &"
+sudo sh -c "cd apache-iotdb2-0.12.2-server-bin/; nohup sbin/start-server.sh &"
