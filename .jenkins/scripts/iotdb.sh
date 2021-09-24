944eecea7aaaefddfcdc9de6434a1d43a4b92daf
Yuan Zi <zy05160516@126.com>
2021-09-24 15:43:48 +0800

    jenkins

---
+++ .jenkins/scripts/iotdb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+sh -c "wget -nv https://mirrors.ocf.berkeley.edu/apache/iotdb/0.11.4/apache-iotdb-0.11.4-bin.zip"
+
+sh -c "unzip apache-iotdb-0.11.4-bin.zip"
+
+sh -c "sleep 10"
+
+sh -c "ls ./"
+
+sh -c "echo ========================="
+
+sh -c "ls apache-iotdb-0.11.4"
+
+sh -c "sudo cp -r apache-iotdb-0.11.4/ apache-iotdb2-0.11.4/"
+
+sh -c "sudo sed -i 's/6667/6668/g' apache-iotdb2-0.11.4/conf/iotdb-engine.properties"
+
+sudo sh -c "cd apache-iotdb-0.11.4/; nohup sbin/start-server.sh &"
+
+sudo sh -c "cd apache-iotdb2-0.11.4/; nohup sbin/start-server.sh &"
