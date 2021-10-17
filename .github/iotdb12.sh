09525c5c365d4b530fe8b4ecc674789b6635a198
unknown <thss15_yit@163.com>
2021-10-17 16:04:02 +0800

    Add test in iotdb12

---
+++ .github/iotdb12.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+sh -c "wget -nv https://dlcdn.apache.org/iotdb/0.12.2/apache-iotdb-0.12.2-server-bin.zip"
+
+sh -c "unzip apache-iotdb-0.12.2-server-bin.zip"
+
+sh -c "sleep 10"
+
+sh -c "ls ./"
+
+sh -c "echo ========================="
+
+sh -c "ls apache-iotdb-0.12.2-server-bin"
+
+sh -c "sudo cp -r apache-iotdb-0.12.2-server-bin/ apache-iotdb2-0.12.2-server-bin"
+
+sh -c "sudo sed -i 's/6667/6668/g' apache-iotdb2-0.12.2-server-bin/conf/iotdb-engine.properties"
+
+sudo sh -c "cd apache-iotdb-0.12.2-server-bin/; nohup sbin/start-all.sh &"
+
+sudo sh -c "cd apache-iotdb2-0.12.2-server-bin/; nohup sbin/start-all.sh &"
