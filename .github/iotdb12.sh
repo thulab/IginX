cdd0310915d6aa914bb223de6142589c77fe4fed
unknown <thss15_yit@163.com>
2021-10-19 16:02:23 +0800

    fix

--- .github/iotdb12.sh
+++ .github/iotdb12.sh
@@ -16,9 +16,9 @@ sh -c "ls apache-iotdb-0.12.2-server-bin"
 
 sh -c "sudo sed -i 's/# wal_buffer_size=16777216/wal_buffer_size=167772160/g' apache-iotdb-0.12.2-server-bin/conf/iotdb-engine.properties"
 
-sh -c "sudo sed -i 's/# wal_buffer_size=16777216/wal_buffer_size=167772160/g' apache-iotdb2-0.12.2-server-bin/conf/iotdb-engine.properties"
-
 sh -c "sudo cp -r apache-iotdb-0.12.2-server-bin/ apache-iotdb2-0.12.2-server-bin"
+
+sh -c "sudo sed -i 's/# wal_buffer_size=16777216/wal_buffer_size=167772160/g' apache-iotdb2-0.12.2-server-bin/conf/iotdb-engine.properties"
 
 sh -c "sudo sed -i 's/6667/6668/g' apache-iotdb2-0.12.2-server-bin/conf/iotdb-engine.properties"
 
