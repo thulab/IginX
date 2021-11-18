0dc94d13f1b58a29d138d0e10b6cedb6a4c12f67
humanfy <humanfy@163.com>
2021-11-19 04:47:49 +0800

    fix restSession

--- core/src/main/java/cn/edu/tsinghua/iginx/rest/RestSession.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/rest/RestSession.java
@@ -24,29 +24,17 @@ import cn.edu.tsinghua.iginx.conf.ConfigDescriptor;
 import cn.edu.tsinghua.iginx.conf.Constants;
 import cn.edu.tsinghua.iginx.exceptions.ExecutionException;
 import cn.edu.tsinghua.iginx.exceptions.SessionException;
-import cn.edu.tsinghua.iginx.session.SessionAggregateQueryDataSet;
 import cn.edu.tsinghua.iginx.session.SessionQueryDataSet;
-import cn.edu.tsinghua.iginx.thrift.AddStorageEnginesReq;
-import cn.edu.tsinghua.iginx.thrift.AggregateQueryReq;
-import cn.edu.tsinghua.iginx.thrift.AggregateQueryResp;
-import cn.edu.tsinghua.iginx.thrift.AggregateType;
 import cn.edu.tsinghua.iginx.thrift.CloseSessionReq;
 import cn.edu.tsinghua.iginx.thrift.DataType;
 import cn.edu.tsinghua.iginx.thrift.DeleteColumnsReq;
 import cn.edu.tsinghua.iginx.thrift.DeleteDataInColumnsReq;
-import cn.edu.tsinghua.iginx.thrift.DownsampleQueryReq;
-import cn.edu.tsinghua.iginx.thrift.DownsampleQueryResp;
-import cn.edu.tsinghua.iginx.thrift.InsertNonAlignedColumnRecordsReq;
-import cn.edu.tsinghua.iginx.thrift.InsertNonAlignedRowRecordsReq;
 import cn.edu.tsinghua.iginx.thrift.InsertRowRecordsReq;
 import cn.edu.tsinghua.iginx.thrift.OpenSessionReq;
 import cn.edu.tsinghua.iginx.thrift.OpenSessionResp;
 import cn.edu.tsinghua.iginx.thrift.QueryDataReq;
 import cn.edu.tsinghua.iginx.thrift.QueryDataResp;
 import cn.edu.tsinghua.iginx.thrift.Status;
-import cn.edu.tsinghua.iginx.thrift.StorageEngine;
-import cn.edu.tsinghua.iginx.thrift.ValueFilterQueryReq;
-import cn.edu.tsinghua.iginx.thrift.ValueFilterQueryResp;
 import cn.edu.tsinghua.iginx.utils.Bitmap;
 import cn.edu.tsinghua.iginx.utils.ByteUtils;
 import cn.edu.tsinghua.iginx.utils.RpcUtils;
@@ -56,15 +44,13 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
 import java.util.*;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
 
 import static cn.edu.tsinghua.iginx.utils.ByteUtils.getByteArrayFromLongArray;
 
 public class RestSession {
     private static final Logger logger = LoggerFactory.getLogger(RestSession.class);
     private static final Config config = ConfigDescriptor.getInstance().getConfig();
-    //private final ReadWriteLock lock;
     private IginxWorker client;
     private long sessionId;
     private boolean isClosed;
@@ -75,7 +61,6 @@ public class RestSession {
     public RestSession() {
         this.isClosed = true;
         this.redirectTimes = 0;
-        //this.lock = new ReentrantReadWriteLock();
         this.username = config.getUsername();
         this.password = config.getPassword();
     }
@@ -138,203 +123,17 @@ public class RestSession {
         return false;
     }
 
-    public void addStorageEngine(String ip, int port, String type, Map<String, String> extraParams) throws ExecutionException {
-        StorageEngine storageEngine = new StorageEngine(ip, port, type, extraParams);
-        AddStorageEnginesReq req = new AddStorageEnginesReq(sessionId, Collections.singletonList(storageEngine));
 
-        Status status;
-        do {
-            /*lock.readLock().lock();
-            try {*/
-                status = client.addStorageEngines(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
-        } while(checkRedirect(status));
-        RpcUtils.verifySuccess(status);
-    }
-
-    public void deleteColumn(String path) throws ExecutionException {
-        List<String> paths = new ArrayList<>();
-        paths.add(path);
-        deleteColumns(paths);
-    }
 
     public void deleteColumns(List<String> paths) throws ExecutionException {
         DeleteColumnsReq req = new DeleteColumnsReq(sessionId, paths);
 
         Status status;
         do {
-            /*lock.readLock().lock();
-            try {*/
                 status = client.deleteColumns(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
         } while(checkRedirect(status));
         RpcUtils.verifySuccess(status);
     }
-
-    public void insertNonAlignedColumnRecords(List<String> paths, long[] timestamps, Object[] valuesList,
-                                              List<DataType> dataTypeList, List<Map<String, String>> attributesList) throws ExecutionException {
-        if (paths.isEmpty() || timestamps.length == 0 || valuesList.length == 0 || dataTypeList.isEmpty()) {
-            logger.error("Invalid insert request!");
-            return;
-        }
-        if (paths.size() != valuesList.length || paths.size() != dataTypeList.size()) {
-            logger.error("The sizes of paths, valuesList and dataTypeList should be equal.");
-            return;
-        }
-        if (attributesList != null && paths.size() != attributesList.size()) {
-            logger.error("The sizes of paths, valuesList, dataTypeList and attributesList should be equal.");
-            return;
-        }
-
-        Integer[] index = new Integer[timestamps.length];
-        for (int i = 0; i < timestamps.length; i++) {
-            index[i] = i;
-        }
-        Arrays.sort(index, Comparator.comparingLong(Arrays.asList(ArrayUtils.toObject(timestamps))::get));
-        Arrays.sort(timestamps);
-        for (int i = 0; i < valuesList.length; i++) {
-            Object[] values = new Object[index.length];
-            for (int j = 0; j < index.length; j++) {
-                values[j] = ((Object[]) valuesList[i])[index[j]];
-            }
-            valuesList[i] = values;
-        }
-
-        List<ByteBuffer> valueBufferList = new ArrayList<>();
-        List<ByteBuffer> bitmapBufferList = new ArrayList<>();
-        for (int i = 0; i < valuesList.length; i++) {
-            Object[] values = (Object[]) valuesList[i];
-            if (values.length != timestamps.length) {
-                logger.error("The sizes of timestamps and the element of valuesList should be equal.");
-                return;
-            }
-            valueBufferList.add(ByteUtils.getColumnByteBuffer(values, dataTypeList.get(i)));
-            Bitmap bitmap = new Bitmap(timestamps.length);
-            for (int j = 0; j < timestamps.length; j++) {
-                if (values[j] != null) {
-                    bitmap.mark(j);
-                }
-            }
-            bitmapBufferList.add(ByteBuffer.wrap(bitmap.getBytes()));
-        }
-
-        InsertNonAlignedColumnRecordsReq req = new InsertNonAlignedColumnRecordsReq();
-        req.setSessionId(sessionId);
-        req.setPaths(paths);
-        req.setTimestamps(getByteArrayFromLongArray(timestamps));
-        req.setValuesList(valueBufferList);
-        req.setBitmapList(bitmapBufferList);
-        req.setDataTypeList(dataTypeList);
-        req.setAttributesList(attributesList);
-
-        Status status;
-        do {
-           /* lock.readLock().lock();
-            try {*/
-                status = client.insertNonAlignedColumnRecords(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
-        } while (checkRedirect(status));
-        RpcUtils.verifySuccess(status);
-    }
-
-    /*public void insertNonAlignedRowRecords(List<String> paths, long[] timestamps, Object[] valuesList,
-                                           List<DataType> dataTypeList, List<Map<String, String>> attributesList) throws ExecutionException {
-        if (paths.isEmpty() || timestamps.length == 0 || valuesList.length == 0 || dataTypeList.isEmpty()) {
-            logger.error("Invalid insert request!");
-            return;
-        }
-        if (paths.size() != dataTypeList.size()) {
-            logger.error("The sizes of paths and dataTypeList should be equal.");
-            return;
-        }
-        if (timestamps.length != valuesList.length) {
-            logger.error("The sizes of timestamps and valuesList should be equal.");
-            return;
-        }
-        if (attributesList != null && paths.size() != attributesList.size()) {
-            logger.error("The sizes of paths, valuesList, dataTypeList and attributesList should be equal.");
-            return;
-        }
-
-        Integer[] index = new Integer[timestamps.length];
-        for (int i = 0; i < timestamps.length; i++) {
-            index[i] = i;
-        }
-        Arrays.sort(index, Comparator.comparingLong(Arrays.asList(ArrayUtils.toObject(timestamps))::get));
-        Arrays.sort(timestamps);
-        Object[] sortedValuesList = new Object[valuesList.length];
-        for (int i = 0; i < valuesList.length; i++) {
-            sortedValuesList[i] = valuesList[index[i]];
-        }
-
-        index = new Integer[paths.size()];
-        for (int i = 0; i < paths.size(); i++) {
-            index[i] = i;
-        }
-        Arrays.sort(index, Comparator.comparing(paths::get));
-        Collections.sort(paths);
-        List<DataType> sortedDataTypeList = new ArrayList<>();
-        List<Map<String, String>> sortedAttributesList = new ArrayList<>();
-        for (int i = 0; i < sortedValuesList.length; i++) {
-            Object[] values = new Object[index.length];
-            for (int j = 0; j < index.length; j++) {
-                values[j] = ((Object[]) sortedValuesList[i])[index[j]];
-            }
-            sortedValuesList[i] = values;
-        }
-        for (Integer i : index) {
-            sortedDataTypeList.add(dataTypeList.get(i));
-        }
-        if (attributesList != null) {
-            for (Integer i : index) {
-                sortedAttributesList.add(attributesList.get(i));
-            }
-        }
-
-        List<ByteBuffer> valueBufferList = new ArrayList<>();
-        List<ByteBuffer> bitmapBufferList = new ArrayList<>();
-        for (int i = 0; i < timestamps.length; i++) {
-            Object[] values = (Object[]) sortedValuesList[i];
-            if (values.length != paths.size()) {
-                logger.error("The sizes of paths and the element of valuesList should be equal.");
-                return;
-            }
-            valueBufferList.add(ByteUtils.getRowByteBuffer(values, sortedDataTypeList));
-            Bitmap bitmap = new Bitmap(values.length);
-            for (int j = 0; j < values.length; j++) {
-                if (values[j] != null) {
-                    bitmap.mark(j);
-                }
-            }
-            bitmapBufferList.add(ByteBuffer.wrap(bitmap.getBytes()));
-        }
-
-        InsertNonAlignedRowRecordsReq req = new InsertNonAlignedRowRecordsReq();
-        req.setSessionId(sessionId);
-        req.setPaths(paths);
-        req.setTimestamps(getByteArrayFromLongArray(timestamps));
-        req.setValuesList(valueBufferList);
-        req.setBitmapList(bitmapBufferList);
-        req.setDataTypeList(sortedDataTypeList);
-        req.setAttributesList(sortedAttributesList);
-
-        Status status;
-        do {
-            *//*lock.readLock().lock();
-            try {*//*
-                status = client.insertNonAlignedRowRecords(req);
-            *//*} finally {
-                lock.readLock().unlock();
-            }*//*
-        } while(checkRedirect(status));
-        RpcUtils.verifySuccess(status);
-    }*/
 
     public void insertRowRecords(List<String> paths, long[] timestamps, Object[] valuesList,
         List<DataType> dataTypeList, List<Map<String, String>> attributesList) throws ExecutionException {
@@ -419,33 +218,18 @@ public class RestSession {
 
         Status status;
         do {
-            /*lock.readLock().lock();
-            try {*/
             status = client.insertRowRecords(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
         } while(checkRedirect(status));
         RpcUtils.verifySuccess(status);
     }
 
-    public void deleteDataInColumn(String path, long startTime, long endTime) {
-        List<String> paths = new ArrayList<>();
-        paths.add(path);
-        deleteDataInColumns(paths, startTime, endTime);
-    }
 
     public void deleteDataInColumns(List<String> paths, long startTime, long endTime) {
         DeleteDataInColumnsReq req = new DeleteDataInColumnsReq(sessionId, paths, startTime, endTime);
 
         Status status;
         do {
-            /*lock.readLock().lock();
-            try {*/
                 status = client.deleteDataInColumns(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
         } while(checkRedirect(status));
     }
 
@@ -454,80 +238,47 @@ public class RestSession {
             logger.error("Invalid query request!");
             return null;
         }
-        QueryDataReq req = new QueryDataReq(sessionId, paths, startTime, endTime);
+        QueryDataReq req = new QueryDataReq(sessionId, mergeAndSortPaths(paths), startTime, endTime);
 
         QueryDataResp resp;
 
         do {
-            /*lock.readLock().lock();
-            try {*/
                 resp = client.queryData(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
         } while(checkRedirect(resp.status));
 
         return new SessionQueryDataSet(resp);
     }
 
-    public SessionQueryDataSet valueFilterQuery(List<String> paths, long startTime, long endTime, String booleanExpression)
-            throws SessionException {
-        if (paths.isEmpty() || startTime > endTime) {
-            logger.error("Invalid query request!");
-            return null;
+    // 适用于查询类请求和删除类请求，因为其 paths 可能带有 *
+    private List<String> mergeAndSortPaths(List<String> paths) {
+        if (paths.stream().anyMatch(x -> x.equals("*"))) {
+            List<String> tempPaths = new ArrayList<>();
+            tempPaths.add("*");
+            return tempPaths;
         }
-        ValueFilterQueryReq req = new ValueFilterQueryReq(sessionId, paths, startTime, endTime, booleanExpression);
-
-        ValueFilterQueryResp resp;
-
-        try {
-            do {
-                /*lock.readLock().lock();
-                try {*/
-                    resp = client.valueFilterQuery(req);
-                /*} finally {
-                    lock.readLock().unlock();
-                }*/
-            } while(checkRedirect(resp.status));
-        } catch (Exception e) {
-            throw new SessionException(e);
+        List<String> prefixes = paths.stream().filter(x -> x.contains("*")).map(x -> x.substring(0, x.indexOf("*"))).collect(Collectors.toList());
+        if (prefixes.isEmpty()) {
+            Collections.sort(paths);
+            return paths;
         }
-
-        return new SessionQueryDataSet(resp);
-    }
-
-    public SessionAggregateQueryDataSet aggregateQuery(List<String> paths, long startTime, long endTime, AggregateType aggregateType) {
-        AggregateQueryReq req = new AggregateQueryReq(sessionId, paths, startTime, endTime, aggregateType);
-
-        AggregateQueryResp resp;
-        do {
-            /*lock.readLock().lock();
-            try {*/
-                resp = client.aggregateQuery(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
-        } while(checkRedirect(resp.status));
-
-        return new SessionAggregateQueryDataSet(resp, aggregateType);
-    }
-
-    public SessionQueryDataSet downsampleQuery(List<String> paths, long startTime, long endTime, AggregateType aggregateType, long precision) {
-        DownsampleQueryReq req = new DownsampleQueryReq(sessionId, paths, startTime, endTime,
-                aggregateType, precision);
-
-        DownsampleQueryResp resp;
-
-        do {
-            /*lock.readLock().lock();
-            try {*/
-                resp = client.downsampleQuery(req);
-            /*} finally {
-                lock.readLock().unlock();
-            }*/
-        } while(checkRedirect(resp.status));
-
-        return new SessionQueryDataSet(resp);
+        List<String> mergedPaths = new ArrayList<>();
+        for (String path : paths) {
+            if (!path.contains("*")) {
+                boolean skip = false;
+                for (String prefix : prefixes) {
+                    if (path.startsWith(prefix)) {
+                        skip = true;
+                        break;
+                    }
+                }
+                if (skip) {
+                    continue;
+                }
+            }
+            mergedPaths.add(path);
+        }
+        mergedPaths.sort(String::compareTo);
+        return mergedPaths;
     }
 
 }
