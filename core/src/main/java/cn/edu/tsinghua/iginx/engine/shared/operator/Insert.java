894720ff1924855a50d173d4a95afddf648497da
Jensen Wang <1092792221@qq.com>
2021-11-25 15:16:29 +0800

    add insert type

--- core/src/main/java/cn/edu/tsinghua/iginx/engine/shared/operator/Insert.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/engine/shared/operator/Insert.java
@@ -5,21 +5,14 @@ import cn.edu.tsinghua.iginx.engine.shared.source.FragmentSource;
 
 public class Insert extends AbstractUnaryOperator {
 
-    private final InsertType insertType;
-
     private final DataSection data;
 
-    public Insert(FragmentSource source, DataSection data, InsertType insertType) {
+    public Insert(FragmentSource source, DataSection data) {
         super(OperatorType.Insert, source);
         if (data == null) {
             throw new IllegalArgumentException("raw data shouldn't be null");
         }
         this.data = data;
-        this.insertType = insertType;
-    }
-
-    public InsertType getInsertType() {
-        return insertType;
     }
 
     public DataSection getData() {
@@ -29,13 +22,6 @@ public class Insert extends AbstractUnaryOperator {
     @Override
     public Operator copy() {
         // data should not be copied in memory.
-        return new Insert((FragmentSource) getSource().copy(), data, getInsertType());
-    }
-
-    public enum InsertType {
-        Column,
-        Row,
-        NonAlignedColumn,
-        NonAlignedRow
+        return new Insert((FragmentSource) getSource().copy(), data);
     }
 }
--- core/src/main/java/cn/edu/tsinghua/iginx/sql/logical/DeleteGenerator.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/sql/logical/DeleteGenerator.java
@@ -1,5 +1,6 @@
 package cn.edu.tsinghua.iginx.sql.logical;
 
+import cn.edu.tsinghua.iginx.conf.ConfigDescriptor;
 import cn.edu.tsinghua.iginx.engine.shared.operator.CombineNonQuery;
 import cn.edu.tsinghua.iginx.engine.shared.operator.Delete;
 import cn.edu.tsinghua.iginx.engine.shared.operator.Operator;
@@ -7,14 +8,23 @@ import cn.edu.tsinghua.iginx.engine.shared.source.OperatorSource;
 import cn.edu.tsinghua.iginx.engine.shared.source.Source;
 import cn.edu.tsinghua.iginx.metadata.DefaultMetaManager;
 import cn.edu.tsinghua.iginx.metadata.IMetaManager;
+import cn.edu.tsinghua.iginx.metadata.entity.FragmentMeta;
+import cn.edu.tsinghua.iginx.metadata.entity.StorageUnitMeta;
+import cn.edu.tsinghua.iginx.metadata.entity.TimeInterval;
+import cn.edu.tsinghua.iginx.metadata.entity.TimeSeriesInterval;
+import cn.edu.tsinghua.iginx.policy.IPolicy;
+import cn.edu.tsinghua.iginx.policy.PolicyManager;
 import cn.edu.tsinghua.iginx.sql.statement.DeleteStatement;
 import cn.edu.tsinghua.iginx.sql.statement.Statement;
 import cn.edu.tsinghua.iginx.sql.statement.StatementType;
+import cn.edu.tsinghua.iginx.utils.Pair;
+import cn.edu.tsinghua.iginx.utils.SortUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class DeleteGenerator implements LogicalGenerator {
 
@@ -27,6 +37,9 @@ public class DeleteGenerator implements LogicalGenerator {
     private final List<Optimizer> optimizerList = new ArrayList<>();
 
     private final static IMetaManager metaManager = DefaultMetaManager.getInstance();
+
+    private final IPolicy policy = PolicyManager.getInstance()
+            .getPolicy(ConfigDescriptor.getInstance().getConfig().getPolicyClassName());
 
     private DeleteGenerator() {
     }
@@ -59,8 +72,24 @@ public class DeleteGenerator implements LogicalGenerator {
     }
 
     private Operator generateRoot(DeleteStatement statement) {
-        List<Delete> deleteList = new ArrayList<>();
+        List<String> pathList = SortUtils.mergeAndSortPaths(new ArrayList<>(statement.getPaths()));
 
+        TimeSeriesInterval interval = new TimeSeriesInterval(pathList.get(0), pathList.get(pathList.size() - 1));
+
+        Map<TimeSeriesInterval, List<FragmentMeta>> fragments = metaManager.getFragmentMapByTimeSeriesInterval(interval);
+        if (fragments.isEmpty()) {
+            Pair<List<FragmentMeta>, List<StorageUnitMeta>> fragmentsAndStorageUnits = policy.getIFragmentGenerator().generateInitialFragmentsAndStorageUnits(pathList, new TimeInterval(0, Long.MAX_VALUE));
+            metaManager.createInitialFragmentsAndStorageUnits(fragmentsAndStorageUnits.v, fragmentsAndStorageUnits.k);
+            fragments = metaManager.getFragmentMapByTimeSeriesInterval(interval);
+        }
+
+        List<Delete> deleteList = new ArrayList<>();
+        fragments.forEach((k, v) -> {
+            v.forEach(fragmentMeta -> {
+                TimeInterval timeInterval = fragmentMeta.getTimeInterval();
+
+            });
+        });
 
         List<Source> sources = new ArrayList<>();
         deleteList.forEach(operator -> sources.add(new OperatorSource(operator)));
