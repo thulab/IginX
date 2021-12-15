7907ba0f4ae30db66fb3c29cdf67b4ce0668350d
Jensen Wang <1092792221@qq.com>
2021-12-15 14:52:40 +0800

    add tests

--- core/src/test/java/cn/edu/tsinghua/iginx/sql/TestUtils.java
+++ core/src/test/java/cn/edu/tsinghua/iginx/sql/TestUtils.java
@@ -1,5 +1,14 @@
 package cn.edu.tsinghua.iginx.sql;
 
+import cn.edu.tsinghua.iginx.engine.shared.operator.BinaryOperator;
+import cn.edu.tsinghua.iginx.engine.shared.operator.MultipleOperator;
+import cn.edu.tsinghua.iginx.engine.shared.operator.Operator;
+import cn.edu.tsinghua.iginx.engine.shared.operator.UnaryOperator;
+import cn.edu.tsinghua.iginx.engine.shared.source.FragmentSource;
+import cn.edu.tsinghua.iginx.engine.shared.source.GlobalSource;
+import cn.edu.tsinghua.iginx.engine.shared.source.OperatorSource;
+import cn.edu.tsinghua.iginx.engine.shared.source.Source;
+import cn.edu.tsinghua.iginx.metadata.entity.FragmentMeta;
 import cn.edu.tsinghua.iginx.sql.statement.Statement;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -14,5 +23,54 @@ public class TestUtils {
         IginXSqlVisitor visitor = new IginXSqlVisitor();
         ParseTree tree = parser.sqlStatement();
         return visitor.visit(tree);
+    }
+
+    public static void print(Operator root) {
+        printOperator(root, "\t");
+    }
+
+    private static void printOperator(Operator operator, String start) {
+        if (operator == null) {
+            return;
+        }
+        String mid = start.substring(0, start.lastIndexOf("\t")) + "└---";
+        System.out.println(mid + operator.getType());
+        if (operator instanceof UnaryOperator) {
+            UnaryOperator uNode = (UnaryOperator) operator;
+            printSource(uNode.getSource(), start + "\t");
+        } else if (operator instanceof BinaryOperator) {
+            BinaryOperator bNode = (BinaryOperator) operator;
+            printSource(bNode.getSourceA(), start + "\t");
+            printSource(bNode.getSourceB(), start + "\t");
+        } else if (operator instanceof MultipleOperator) {
+            MultipleOperator mNode = (MultipleOperator) operator;
+            for (Source source : mNode.getSources()) {
+                printSource(source, start + "\t");
+            }
+        }
+    }
+
+    private static void printSource(Source source, String start) {
+        if (source instanceof OperatorSource) {
+            OperatorSource opSource = (OperatorSource) source;
+            printOperator(opSource.getOperator(), start);
+        } else {
+            String mid = start.substring(0, start.lastIndexOf("\t")) + "└---";
+            if (source instanceof FragmentSource) {
+                FragmentSource fSource = (FragmentSource) source;
+                System.out.println(mid + formatFragment(fSource.getFragment()));
+            } else if (source instanceof GlobalSource) {
+                System.out.println(mid + "global");
+            }
+        }
+    }
+
+    private static String formatFragment(FragmentMeta fragment) {
+        return "time[" +
+                fragment.getTimeInterval().getStartTime() + ", " +
+                fragment.getTimeInterval().getEndTime() + "), " +
+                "ts[" +
+                fragment.getTsInterval().getStartTimeSeries() + ", " +
+                fragment.getTsInterval().getEndTimeSeries() + ")";
     }
 }
