8e4695b699ba5613c68216a28db8c2b22c44e613
Yuan Zi <zy05160516@126.com>
2022-05-24 19:54:11 +0800

    fix

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/history/IoTDBHistoryDataGeneratorTest.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/history/IoTDBHistoryDataGeneratorTest.java
@@ -2,8 +2,12 @@ package cn.edu.tsinghua.iginx.integration.history;
 
 import org.apache.iotdb.session.Session;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IoTDBHistoryDataGeneratorTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(IoTDBHistoryDataGeneratorTest.class);
 
     @Test
     public void writeHistoryDataToA() throws Exception {
@@ -14,6 +18,8 @@ public class IoTDBHistoryDataGeneratorTest {
         session.executeNonQueryStatement("INSERT INTO root.ln.wf01.wt01(timestamp,status,temperature) values(200,false,20.71);");
 
         session.close();
+
+        logger.info("write data to 127.0.0.1:6667 success!");
     }
 
     @Test
@@ -25,6 +31,8 @@ public class IoTDBHistoryDataGeneratorTest {
         session.executeNonQueryStatement("INSERT INTO root.ln.wf03.wt01(timestamp,status,temperature) values(200,false,77.71);");
 
         session.close();
+
+        logger.info("write data to 127.0.0.1:6667 success!");
     }
 
 }
