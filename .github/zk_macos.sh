170b34bf30461c30b582aac2939072deabdfc125
SolomonAnn <thss15_anyz@163.com>
2022-09-23 11:01:31 +0800

    Debug

--- .github/zk_macos.sh
+++ .github/zk_macos.sh
@@ -12,6 +12,8 @@ sh -c "cp zookeeper/conf/zoo_sample.cfg zookeeper/conf/zoo.cfg"
 
 sh -c "sed -i '' 's/#maxClientCnxns=60/maxClientCnxns=60/' zookeeper/conf/zoo.cfg"
 
+sh -c "/usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate"
+
 sh -c "cat zookeeper/conf/zoo.cfg"
 
 sh -c "zookeeper/bin/zkServer.sh start"
