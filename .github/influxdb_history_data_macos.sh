db460e3bb6290202b5309c89afa3fd7263874abf
SolomonAnn <thss15_anyz@163.com>
2022-09-23 19:51:24 +0800

    Debug

--- .github/influxdb_history_data_macos.sh
+++ .github/influxdb_history_data_macos.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-sh -c "wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.7-linux-amd64.tar.gz"
+sh -c "wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.7-darwin-amd64.tar.gz"
 
-sh -c "tar -zxvf influxdb2-2.0.7-linux-amd64.tar.gz"
+sh -c "tar -zxvf influxdb2-2.0.7-darwin-amd64.tar.gz"
 
-sh -c "ls influxdb2-2.0.7-linux-amd64"
+sh -c "ls influxdb2-2.0.7-darwin-amd64"
 
-sudo sh -c "cd influxdb2-2.0.7-linux-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 --query-memory-bytes=20971520 &"
+sudo sh -c "cd influxdb2-2.0.7-darwin-amd64/; nohup ./influxd run --bolt-path=~/.influxdbv2/influxd.bolt --engine-path=~/.influxdbv2/engine --http-bind-address=:8086 --query-memory-bytes=20971520 &"
 
 sh -c "sleep 30"
 
-sh -c "./influxdb2-2.0.7-linux-amd64/influx setup --org testOrg --bucket testBucket --username user --password 12345678 --token testToken --force"
+sh -c "./influxdb2-2.0.7-darwin-amd64/influx setup --org testOrg --bucket testBucket --username user --password 12345678 --token testToken --force"
 
 sed -i "" "s/your-token/testToken/" conf/config.properties
 
