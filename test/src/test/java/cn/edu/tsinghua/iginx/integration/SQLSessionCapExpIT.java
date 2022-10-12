6df98d56a141a0db3e21c9b5dccc5e9bbec7354b
RemHero <1104304963@qq.com>
2022-10-12 15:44:07 +0800

    debug

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/SQLSessionCapExpIT.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/SQLSessionCapExpIT.java
@@ -1047,7 +1047,7 @@ public abstract class SQLSessionCapExpIT {
         String delete = "DELETE FROM us.d1.s1 WHERE time > 205 AND time < 215 OR time >= 210 AND time <= 230;";
         execute(delete);
 
-        String queryOverDeleteRange = "SELECT s1 FROM us.d1 WHERE time > 200 AND time < 235;";
+        String queryOverDeleteRange = "SELECT s1 FROM us.d1";
         String expected = "ResultSets:\n" +
                 "+----+--------+\n" +
                 "|Time|us.d1.s1|\n" +
