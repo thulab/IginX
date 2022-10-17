b313ba5198d457492d057ea78996688b953b2200
Yuan Zi <zy05160516@126.com>
2022-10-17 20:55:46 +0800

    fix wildcard in initial fragment

--- core/src/main/java/cn/edu/tsinghua/iginx/policy/Utils.java
+++ core/src/main/java/cn/edu/tsinghua/iginx/policy/Utils.java
@@ -1,12 +1,12 @@
 package cn.edu.tsinghua.iginx.policy;
 
+import cn.edu.tsinghua.iginx.conf.Constants;
 import cn.edu.tsinghua.iginx.sql.statement.DataStatement;
 import cn.edu.tsinghua.iginx.sql.statement.DeleteStatement;
 import cn.edu.tsinghua.iginx.sql.statement.InsertStatement;
 import cn.edu.tsinghua.iginx.sql.statement.SelectStatement;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Utils {
 
@@ -19,6 +19,33 @@ public class Utils {
             case INSERT:
                 return ((InsertStatement) statement).getPaths();
         }
-        return null;
+        return Collections.emptyList();
     }
+
+    public static List<String> getNonWildCardPaths(List<String> paths) {
+        Set<String> beCutPaths = new HashSet<>();
+        for (String path: paths) {
+            if (!path.contains(Constants.LEVEL_PLACEHOLDER)) {
+                beCutPaths.add(path);
+                continue;
+            }
+            String[] parts = path.split("\\" + Constants.LEVEL_SEPARATOR);
+            if (parts.length == 0 || parts[0].equals(Constants.LEVEL_PLACEHOLDER)) {
+                continue;
+            }
+            StringBuilder pathBuilder = new StringBuilder();
+            for (String part: parts) {
+                if (part.equals(Constants.LEVEL_PLACEHOLDER)) {
+                    break;
+                }
+                if (pathBuilder.length() != 0) {
+                    pathBuilder.append(Constants.LEVEL_SEPARATOR);
+                }
+                pathBuilder.append(part);
+            }
+            beCutPaths.add(pathBuilder.toString());
+        }
+        return new ArrayList<>(beCutPaths);
+    }
+
 }
