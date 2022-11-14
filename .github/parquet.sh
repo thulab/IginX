a26a64ea0ea7e66b7d4e07362383d43ab5db3db4
Jensen Wang <1092792221@qq.com>
2022-11-14 11:12:55 +0800

    fix actions

--- .github/parquet.sh
+++ .github/parquet.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sed -i "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/g" conf/config.properties
+sed -i "s/storageEngineList=127.0.0.1#6667#iotdb11/#storageEngineList=127.0.0.1#6667#iotdb11/g" conf/config.properties
 
-sed -i "s/#storageEngineList=127.0.0.1#6667#parquet#dir=parquetData#isLocal=true/storageEngineList=127.0.0.1#6667#parquet#dir=parquetData#isLocal=true/g" conf/config.properties
+sed -i "s/#storageEngineList=127.0.0.1#6667#parquet/storageEngineList=127.0.0.1#6667#parquet/g" conf/config.properties
 
 sed -i "s/enablePushDown=true/enablePushDown=false/g" conf/config.properties
--- .github/parquet_macos.sh
+++ .github/parquet_macos.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sed -i "" "s/storageEngineList=127.0.0.1#6667/#storageEngineList=127.0.0.1#6667/" conf/config.properties
+sed -i "" "s/storageEngineList=127.0.0.1#6667#iotdb11/#storageEngineList=127.0.0.1#6667#iotdb11/" conf/config.properties
 
-sed -i "" "s/#storageEngineList=127.0.0.1#6667#parquet#dir=parquetData#isLocal=true/storageEngineList=127.0.0.1#6667#parquet#dir=parquetData#isLocal=true/" conf/config.properties
+sed -i "" "s/#storageEngineList=127.0.0.1#6667#parquet/storageEngineList=127.0.0.1#6667#parquet/" conf/config.properties
 
 sed -i "" "s/enablePushDown=true/enablePushDown=false/" conf/config.properties
