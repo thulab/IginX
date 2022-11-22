0630eb72e60287411651ad04dad564ba54c5babe
Yuan Zi <zy05160516@126.com>
2022-11-22 08:52:26 +0800

    fix

--- sync/src/main/java/cn/edu/tsinghua/iginx/proposal/ProposalListener.java
+++ sync/src/main/java/cn/edu/tsinghua/iginx/proposal/ProposalListener.java
@@ -2,10 +2,26 @@ package cn.edu.tsinghua.iginx.proposal;
 
 public interface ProposalListener {
 
+    /**
+     * when proposal created, this method will be called.
+     * @param key proposal key
+     * @param syncProposal proposal content
+     */
     void onCreate(String key, SyncProposal syncProposal);
 
+    /**
+     * when proposal updated, this method will be called.
+     * @param key proposal key
+     * @param beforeSyncProposal proposal content before update
+     * @param afterSyncProposal proposal content after update
+     */
     void onUpdate(String key, SyncProposal beforeSyncProposal, SyncProposal afterSyncProposal);
 
+    /**
+     * when proposal deleted, this method will be called.
+     * @param key proposal key
+     * @param syncProposal proposal content
+     */
     void onDelete(String key, SyncProposal syncProposal);
 
 }
--- sync/src/main/java/cn/edu/tsinghua/iginx/protocol/SyncProtocol.java
+++ sync/src/main/java/cn/edu/tsinghua/iginx/protocol/SyncProtocol.java
@@ -7,12 +7,33 @@ import cn.edu.tsinghua.iginx.proposal.VoteListener;
 
 public interface SyncProtocol {
 
+    /**
+     * start a sync proposal
+     * @param key proposal key
+     * @param syncProposal proposal content
+     * @param listener vote listener
+     * @return success or failure
+     */
     boolean startProposal(String key, SyncProposal syncProposal, VoteListener listener) throws NetworkException;
 
+    /**
+     * register proposal listener, when proposal create/update/delete, listener will receive notification
+     * @param listener proposal listener
+     */
     void registerProposalListener(ProposalListener listener) ;
 
+    /**
+     * vote for proposal
+     * @param key proposal key
+     * @param vote proposal vote
+     */
     void voteFor(String key, SyncVote vote) throws NetworkException, VoteExpiredException;
 
+    /**
+     * end proposal
+     * @param key proposal key
+     * @param syncProposal proposal content
+     */
     void endProposal(String key, SyncProposal syncProposal) throws NetworkException, ExecutionException;
 
 }
