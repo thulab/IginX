6f6f4569a947c5446ec433a4ae2e927b070dd567
RemHero <1104304963@qq.com>
2023-01-28 18:28:09 +0800

    update zk address

--- .github/zk_macos.sh
+++ .github/zk_macos.sh
@@ -4,9 +4,9 @@ set -e
 
 sh -c "wget -nv https://mirror.softaculous.com/apache/zookeeper/zookeeper-3.6.4/apache-zookeeper-3.6.4-bin.tar.gz --no-check-certificate"
 
-sh -c "tar -xzf apache-zookeeper-3.6.3-bin.tar.gz"
+sh -c "tar -xzf apache-zookeeper-3.6.4-bin.tar.gz"
 
-sh -c "mv apache-zookeeper-3.6.3-bin zookeeper"
+sh -c "mv apache-zookeeper-3.6.4-bin zookeeper"
 
 sh -c "cp ./.github/actions/zookeeperRunner/zooMac.cfg zookeeper/conf/zoo.cfg"
 
